c524d8404f62fbe5fd74bd74c5a07acc2c58afc1
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2021-06-05 08:58:53 +0530

    modified tracker.py

--- tracker.py
+++ tracker.py
@@ -8,8 +8,10 @@ import os
 
 
 class VaccineTracker:
-    def __init__(self,state_id):
-
+    '''Tracks Vaccine Slots for the given state and districts'''
+    def __init__(self,state_id=9):
+        '''Default State ID is set to Delhi i.e. 9. 
+        To check your State's ID call the class without any parameters and run the get_state_id function'''
         self.cowin=CoWinAPI()
         self.states=self.cowin.get_states()
         self.districts=self.cowin.get_districts(state_id)
@@ -32,28 +34,42 @@ class VaccineTracker:
 
 
     def check_availability_by_district(self,district_name,age=None,location_list=[]):
-        
+        '''Gives the Number of Slots available in a district with the age that is specified'''
 
         dis_id=self.get_district_id(district_name)
+
+        # Checks the Availabilty for next 45 days
         for no in range(45):
             date_for_vaccine=(datetime.datetime.today() + datetime.timedelta(no)).strftime('%d-%m-%Y')
             available_location=self.cowin.get_availability_by_district(district_id=str(dis_id),min_age_limt=age,date=date_for_vaccine)['centers']
+            
+            # Iterating through available locations
             for location in available_location:
+
+                # Checking if there are available slots. i.e. available_capacity not equal to 0
                 if location['sessions'][0]['available_capacity'] !=0:
                     location_list.append({'Date':date_for_vaccine,'District':district_name.upper(),'Location':location['name'],'Pincode':location['pincode'],'Availability':location['sessions'][0]['available_capacity'],'Vaccine':location['sessions'][0]['vaccine']})
+        
+        # Returning list of locations where slots are available
         return location_list
 
+
+def get_states_with_id():
+    '''Returns Nested List with ID and STATE'''
+
+    records=[]
+    records.append('{:>3} | {:>3}'.format('ID','STATE'))
+
+    for state in CoWinAPI().get_states()['states']:
+        records.append('{:>3} | {:>3}'.format(state['state_id'],state['state_name']))
+        
+    return records                
+
         
         
-        
-
-
-
-
-
 if __name__=='__main__':
-
-    tracker=VaccineTracker(16)
+    
+    tracker=VaccineTracker()
     locations=[]
     while True:
         areas=['bangalore rural','bangalore urban','bbmp']
@@ -75,6 +91,8 @@ if __name__=='__main__':
                     
             f.close()
         os.remove('Vaccine.csv')
-        time.sleep(1800)
-        locations=[]
+        
+    
+        
+        
     
